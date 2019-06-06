66697d76e083d48c3dcf8eca0ab46f66cddd7dd4
Paul Hammant <paul@hammant.org>
2019-06-06 06:46:54 +0100

    one more way in

---
+++ src/main/java/com/paulhammant/svnmerkleizer/boot/ViaHiddenGetRoutesAndCommandLineArgs.java
@@ -0,0 +1,20 @@
+package com.paulhammant.svnmerkleizer.boot;
+
+import com.paulhammant.svnmerkleizer.SubversionDirectoryMerkleizerService;
+import com.paulhammant.svnmerkleizer.SvnMerkleizer;
+
+public class ViaHiddenGetRoutesAndCommandLineArgs {
+
+    public static void main(String[] args) {
+        final SvnMerkleizer.Metrics.Console metrics1 = new SvnMerkleizer.Metrics.Console();
+        final String delegateTo = args[0];
+        final String contextDir = args[1];
+        final String cacheFilePath = args[2];
+        final String port = args[3];
+        new SubversionDirectoryMerkleizerService.ViaHiddenGetRoutes(delegateTo, contextDir, metrics1,
+                new SvnMerkleizer(delegateTo, contextDir, metrics1, cacheFilePath),
+                Integer.parseInt(port)
+        ).start(args);
+    }
+
+}
